5897d79a46d4a494bcaf37730b2ab4ce0eb94700
Kevin Hintz <khintz34@gmail.com>
2023-04-05 10:54:34 -0500

    mobile

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -4,46 +4,48 @@ import "./Tooltip.css";
 const Tooltip = () => {
   return (
     <div id="mainContainer">
-      <div className="tipContainer flexRight ">
-        <p className="tooltip pTopLeft aLeft">
-          Top
-          <span className="tooltipText">text</span>
-        </p>
-      </div>
-      <div className="tipContainer flexLeft">
-        <p className="tooltip pTopRight  aRight">
-          Top <span className="tooltipText">text</span>
-        </p>
-      </div>
-      <div className="tipContainer">
-        <p className="tooltip pLeft">
-          Left <span className="tooltipText">text</span>
-        </p>
-      </div>
-      <div className="tipContainer">
-        <p className="tooltip pRight">
-          Right <span className="tooltipText">text</span>
-        </p>
-      </div>
-      <div className="tipContainer">
-        <p className="tooltip pLeft">
-          Left <span className="tooltipText">text</span>
-        </p>
-      </div>
-      <div className="tipContainer">
-        <p className="tooltip pRight">
-          Right <span className="tooltipText">text</span>
-        </p>
-      </div>
-      <div className="tipContainer flexRight">
-        <p className="tooltip pBotLeft">
-          Bottom <span className="tooltipText">text</span>
-        </p>
-      </div>
-      <div className="tipContainer flexLeft">
-        <p className="tooltip pBotRight">
-          Bottom <span className="tooltipText">text</span>
-        </p>
+      <div id="mainTip">
+        <div className="tipContainer flexRight ">
+          <p className="tooltip pTopLeft aLeft">
+            Top
+            <span className="tooltipText">text</span>
+          </p>
+        </div>
+        <div className="tipContainer flexLeft">
+          <p className="tooltip pTopRight  aRight">
+            Top <span className="tooltipText">text</span>
+          </p>
+        </div>
+        <div className="tipContainer">
+          <p className="tooltip pLeft">
+            Left <span className="tooltipText">text</span>
+          </p>
+        </div>
+        <div className="tipContainer">
+          <p className="tooltip pRight">
+            Right <span className="tooltipText">text</span>
+          </p>
+        </div>
+        <div className="tipContainer">
+          <p className="tooltip pLeft">
+            Left <span className="tooltipText">text</span>
+          </p>
+        </div>
+        <div className="tipContainer">
+          <p className="tooltip pRight">
+            Right <span className="tooltipText">text</span>
+          </p>
+        </div>
+        <div className="tipContainer flexRight">
+          <p className="tooltip pBotLeft">
+            Bottom <span className="tooltipText">text</span>
+          </p>
+        </div>
+        <div className="tipContainer flexLeft">
+          <p className="tooltip pBotRight">
+            Bottom <span className="tooltipText">text</span>
+          </p>
+        </div>
       </div>
     </div>
   );
